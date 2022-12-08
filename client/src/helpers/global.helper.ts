ecec0e4d731a40094b7c41c91716c948b7872ec8
Gideon Idoko <iamgideonidoko@gmail.com>
2022-12-08 05:11:50 +0100

    feat: add image helper function

--- client/src/helpers/global.helper.ts
+++ client/src/helpers/global.helper.ts
@@ -6,3 +6,18 @@ export const removeObjectProps = <TData extends { [k: string]: unknown }, TRes =
 };
 
 export const getImgStringfromBase64Data = (base64ImageData: string) => `data:image/png;base64,${base64ImageData}`;
+
+/**
+ * Convert image URI to uploadable blob
+ * const file = dataURIToBlob(image_uri);
+ */
+export const dataURIToBlob = (dataURI: string) => {
+  const splitDataURI = dataURI.split(',');
+  const byteString = splitDataURI[0].indexOf('base64') >= 0 ? atob(splitDataURI[1]) : decodeURI(splitDataURI[1]);
+  const mimeString = splitDataURI[0].split(':')[1].split(';')[0];
+
+  const ia = new Uint8Array(byteString.length);
+  for (let i = 0; i < byteString.length; i++) ia[i] = byteString.charCodeAt(i);
+
+  return new Blob([ia], { type: mimeString });
+};
